d58df24d0e990d074476cef048da73b8d89a100c
Yamila Rosales <Yamila.Rosales@lexicacorp.com>
2021-11-08 21:17:20 -0300

    Initial upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Plantas vs Zombies - Cursos para el cuidado de tus plantas de interior
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ingresá y que tu potus deje de ser un zombie!
         </a>
       </header>
     </div>
