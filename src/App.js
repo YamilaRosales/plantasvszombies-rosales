8c6e33b586142ed63e0bdd5ebd8fe03a5d0ae427
Yamila Rosales <Yamila.Rosales@lexicacorp.com>
2021-11-18 21:57:25 -0300

    Add ItemListContaner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
+  let listItems=["Santa Rita","Potus", "Peperomia", "Suculenta","Malvones"];
   return (
     <div className="App">
       <NavBar/>    
-      <header className="App-header">
-       
-      </header>
+      <ItemListContainer items={listItems} description="Lista"/>
     </div>
   );
 }
