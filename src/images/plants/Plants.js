02cc2e1772a82e2b91940c9b494488cab3110c9a
Yamila Rosales <Yamila.Rosales@lexicacorp.com>
2021-11-25 08:25:18 -0300

    Add images references

---
+++ src/images/plants/Plants.js
@@ -0,0 +1,9 @@
+const PLANTS = {
+    malvon: require('./Malvon.jpg').default,
+    peperomia: require('./PeperomiaSandia.jpg').default,
+    potus: require('./Potus.jpg').default,
+    santaRita: require('./SantaRita.jpg').default,
+    suculenta: require('./Suculenta.jpg').default
+}
+
+export default PLANTS;
