68b3555b1a65214f526aa424a652b784f280452c
Yamila Rosales <Yamila.Rosales@lexicacorp.com>
2021-12-15 01:05:26 -0300

    add totalQty

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,9 +1,18 @@
 import { BsFillCartFill } from "react-icons/bs";
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
 const CartWidget = () => {
-
-    return( 
-        <BsFillCartFill size={25} color="#282c34"/>
-    )
-}
+  const [items, addItem, removeItem, clear] = useContext(CartContext);
+  let totalQty = 0;
+  items.forEach((element) => {
+    totalQty += element.qty;
+  });
+  return (
+    <>
+      {totalQty}
+      <BsFillCartFill size={25} color="#282c34" />
+    </>
+  );
+};
 export default CartWidget;
