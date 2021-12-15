bed1aecde4bd68567e9dbae3920a5a7c90805add
Yamila Rosales <Yamila.Rosales@lexicacorp.com>
2021-12-15 01:04:56 -0300

    Add delete button, and bootstrap style table

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,60 @@
 import React, { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { Button, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Cart() {
   const [items, addItem, removeItem, clear] = useContext(CartContext);
 
   console.log("items en Cart:", items);
+
+  const handlerRemoveItem = (itemId) => {
+    removeItem(itemId);
+  };
+  let total = 0;
   const itemsAdded = items.map(function (item) {
+    total += item.qty * item.item.price;
     return (
-      <div key={item.item.id}>
-        <div>{item.item.name}</div>
-        <div>{item.item.price}</div>
-      </div>
+      <tr key={item.item.id}>
+        <td>{item.item.name}</td>
+        <td>{item.item.price}</td>
+        <td>{item.qty}</td>
+        <td>
+          <Button onClick={() => handlerRemoveItem(item.item.id)}>
+            Eliminar
+          </Button>
+        </td>
+      </tr>
     );
   });
 
   return (
     <div>
-      {items && items.length > 0 ? itemsAdded : "No existen items agregados"}
+      {items && items.length > 0 ? (
+        <div>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>{itemsAdded}</tbody>
+          </Table>
+          <h2>Total: ${total}</h2>
+        </div>
+      ) : (
+        "No existen items agregados"
+      )}
+      <div>
+        <Link to="/">
+          <Button variant="primary" size="sm" active>
+            Volver
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -1,40 +1,40 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-export const CartContext = createContext ([])
+export const CartContext = createContext([]);
 
+const CartProvider = ({ defaultValue = [], children }) => {
+  const [items, setItems] = useState([]);
 
-const CartProvider = ({defaultValue=[], children}) =>{
-    const [items, setItems] = useState([])
+  const isInCart = (id) => {
+    return items.some((i) => (i.id = id));
+  };
 
-    const isInCart = (id) => {
-        return items.some(i => i.id = id);
-    }
-    
+  const sum = (id, qty) => {
+    let itemsAux = items;
+    itemsAux.forEach(function (item) {
+      if (item.item.id === id) item.qty += qty;
+    });
+    setItems(itemsAux);
+  };
+  const addItem = (item, qty) => {
+    if (isInCart(item.id)) sum(item.id, qty);
+    else setItems([...items, { item, qty: qty }]);
+    console.log(items);
+  };
 
-    const sum = (id, qty) => {
-        let itemsAux = items;
-        itemsAux.forEach(function(item){
-           if(item.item.id === id)   
-                item.qty+=qty;
-        });
-        setItems(itemsAux);
-    }
-    const addItem =  (item, qty)=>{
-        if(isInCart(item.id)) 
-            sum(item.id, qty);
-        else
-            setItems([...items, {item, qty: qty}]);
-        console.log(items);
-    }
-    
-    const removeItem = (itemId)=>{
-        return isInCart(itemId) ? console.log("No se encontro el item a eliminar"): items.filter(item => item.id !== itemId);
-    }
+  const removeItem = (itemId) => {
+    if (!isInCart(itemId)) console.log("No se encontro el item a eliminar");
+    else setItems(items.filter((item) => item.id !== itemId));
+  };
 
-    const clear =()=>{
-        return items.length =0;
-    }
-    
-    return <CartContext.Provider value={[items, addItem, removeItem, clear ]}>{children}</CartContext.Provider>
-}
+  const clear = () => {
+    return (items.length = 0);
+  };
+
+  return (
+    <CartContext.Provider value={[items, addItem, removeItem, clear]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
 export default CartProvider;
